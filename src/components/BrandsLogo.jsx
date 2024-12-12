2118aebb4a300fe60cdba590de1e1c4d229d5bcf
Iman Jalali <i.jalali88@gmail.com>
2024-12-12 16:02:32 +0330

    brands section

--- src/components/BrandsLogo.jsx
+++ src/components/BrandsLogo.jsx
@@ -2,8 +2,14 @@ import MyIcon from "./MyIcon";
 
 export default function BrandsLogo() {
   return (
-    <div className="container mx-auto w-[70%]">
-      <MyIcon iconName="zapier" />
+    <div className="container mx-auto w-[55%] flex items-center justify-between mt-28">
+      <MyIcon size="85" iconName="zapier" />
+      <MyIcon size="85" iconName="pipedrive" />
+      <MyIcon size="85" iconName="cib-bank" />
+      <MyIcon size="85" iconName="Logo" />
+      <MyIcon size="85" iconName="burnt-toast" />
+      <MyIcon size="85" iconName="panda-doc" />
+      <MyIcon size="85" iconName="moz" />
     </div>
   );
 }
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import BrandsLogo from "../components/BrandsLogo";
 
 export default function HomePage() {
   return (
     <>
       <Banner />
+      <BrandsLogo />
     </>
   );
 }
