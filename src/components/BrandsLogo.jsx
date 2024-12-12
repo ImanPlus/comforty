c8443e6d52cf10043ac36e9867d727a9646a972a
Iman Jalali <i.jalali88@gmail.com>
2024-12-12 14:56:04 +0330

    save pictures of logo

---
+++ src/components/BrandsLogo.jsx
@@ -0,0 +1,9 @@
+import MyIcon from "./MyIcon";
+
+export default function BrandsLogo() {
+  return (
+    <div className="container mx-auto w-[70%]">
+      <MyIcon iconName="zapier" />
+    </div>
+  );
+}
