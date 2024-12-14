d74ea1195b244dcde5b9234f3f4081ffb3c6189c
Iman Jalali <i.jalali88@gmail.com>
2024-12-14 12:36:49 +0330

    create data resource in constants

---
+++ src/constants/data.js
@@ -0,0 +1,80 @@
+import chair1 from "../assets/chair-1.png";
+import chair2 from "../assets/chair-2.png";
+import chair3 from "../assets/chair-3.png";
+import chair4 from "../assets/chair-4.png";
+import topCh01 from "../assets/top-ch-1.png";
+import topCh02 from "../assets/top-ch-2.png";
+import topCh03 from "../assets/top-ch-3.png";
+import topCh04 from "../assets/top-ch-4.jpeg";
+import topCh05 from "../assets/top-ch-5.png";
+import topCh06 from "../assets/top-ch-6.png";
+
+export const card_1 = [
+  {
+    image: chair1,
+    title: "Library Stool Chair",
+    price: 20,
+    lable: "New",
+    favorite: "true",
+    unSelectCart: false,
+  },
+  {
+    image: chair2,
+    title: "Office Chair",
+    price: 35,
+    lable: "Sales",
+  },
+  {
+    image: chair3,
+    title: "Gaming Chair",
+    price: 50,
+  },
+  {
+    image: chair4,
+    title: "Dining Chair",
+    price: 25,
+  },
+  {
+    image: topCh01,
+    title: "Library Stool Chair",
+    price: 20,
+  },
+  {
+    image: topCh05,
+    title: "Library Stool Chair",
+    price: 20,
+  },
+  {
+    image: topCh06,
+    title: "Library Stool Chair",
+    price: 20,
+  },
+  {
+    image: chair1,
+    title: "Library Stool Chair",
+    price: 20,
+  },
+];
+
+export const card_2 = [
+  {
+    image: topCh01,
+    title: "Wing Chair",
+    count: "3,584",
+  },
+  {
+    image: topCh02,
+    title: "Wooden Chair",
+    count: "157",
+  },
+  {
+    image: topCh03,
+    title: "Desk Chair",
+    price: "154",
+  },
+  {
+    image: topCh04,
+    title: "Park Bench",
+    price: "154",
+  },
+];
